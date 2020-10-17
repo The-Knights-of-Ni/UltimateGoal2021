59c9d9f777c42b65ad793ee4986d187aa3b936a9
Paige Yeung <paigeyeung@gmail.com>
2020-10-17 15:17:23 -0700

    Set up initial files, copy relevant code from Skystone

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Robot.java
@@ -0,0 +1,352 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorControllerEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.SubSystems.Drive;
+import org.firstinspires.ftc.teamcode.SubSystems.Vision;
+
+import java.io.IOException;
+
+/**
+ * Created by AndrewC on 12/27/2019.
+ */
+
+public class Robot extends Subsystem {
+    public String name;
+    private HardwareMap hardwareMap;
+    private LinearOpMode opMode;
+    public ElapsedTime timer;
+
+    //DC Motors
+    public DcMotorEx frontLeftDriveMotor;
+    public DcMotorEx frontRightDriveMotor;
+    public DcMotorEx rearRightDriveMotor;
+    public DcMotorEx rearLeftDriveMotor;
+    public DcMotorEx xRailWinch;
+    public DcMotorEx armTilt;
+
+    //Servos
+    public Servo mainClawArm;
+    public Servo mainClawRotation;
+    public Servo mainClaw; //0
+    public Servo csClaw; //capstone claw
+    public Servo csArm; //capstone arm
+    public Servo fClawL; //foundationClawLeft
+    public Servo fClawR; // foundationClawRight
+
+    /**
+     * HUB2
+     * fl   0
+     * fr   1
+     * bl   2
+     * br   3
+     *
+     * mA (mainArm)         2
+     * mR (mainRotation)    1
+     * mC (mainClaw)        0
+     * csC (csClaw)         3
+     * csA (csArm)          4
+     * --------------------
+     * HUB1
+     * tilt     0
+     * winch    1
+     *
+     * fL (fClawL)          2
+     * fR (fClawR)          1
+     */
+
+    //Sensors
+    public BNO055IMU imu;
+    private ColorSensor colorSensor;
+
+    // Declare game pad objects
+    public double leftStickX;
+    public double leftStickY;
+    public double rightStickX;
+    public double rightStickY;
+    public float triggerLeft;
+    public float triggerRight;
+    public boolean aButton = false;
+    public boolean bButton = false;
+    public boolean xButton = false;
+    public boolean yButton = false;
+    public boolean dPadUp = false;
+    public boolean dPadDown = false;
+    public boolean dPadLeft = false;
+    public boolean dPadRight = false;
+    public boolean bumperLeft = false;
+    public boolean bumperRight = false;
+
+    public double leftStickX2;
+    public double leftStickY2;
+    public double rightStickX2;
+    public double rightStickY2;
+    public float triggerLeft2;
+    public float triggerRight2;
+    public boolean aButton2 = false;
+    public boolean bButton2 = false;
+    public boolean xButton2 = false;
+    public boolean yButton2 = false;
+    public boolean dPadUp2 = false;
+    public boolean dPadDown2 = false;
+    public boolean dPadLeft2 = false;
+    public boolean dPadRight2 = false;
+    public boolean bumperLeft2 = false;
+    public boolean bumperRight2 = false;
+
+    public boolean isaButtonPressedPrev = false;
+    public boolean isbButtonPressedPrev = false;
+    public boolean isxButtonPressedPrev = false;
+    public boolean isyButtonPressedPrev = false;
+    public boolean isdPadUpPressedPrev = false;
+    public boolean isdPadDownPressedPrev = false;
+    public boolean isdPadLeftPressedPrev = false;
+    public boolean isdPadRightPressedPrev = false;
+    public boolean islBumperPressedPrev = false;
+    public boolean isrBumperPressedPrev = false;
+    public boolean isaButton2PressedPrev = false;
+    public boolean isbButton2PressedPrev = false;
+    public boolean isxButton2PressedPrev = false;
+    public boolean isyButton2PressedPrev = false;
+    public boolean isdPadUp2PressedPrev = false;
+    public boolean isdPadDown2PressedPrev = false;
+    public boolean isdPadLeft2PressedPrev = false;
+    public boolean isdPadRight2PressedPrev = false;
+    public boolean islBumper2PressedPrev = false;
+    public boolean isrBumper2PressedPrev = false;
+
+    private double joystickDeadZone = 0.1;
+
+    //Subsystems
+    public Drive drive;
+    public Control control;
+    public Vision vision;
+
+    public Robot(LinearOpMode opMode, ElapsedTime timer) throws IOException {
+        hardwareMap = opMode.hardwareMap;
+        this.opMode = opMode;
+        this.timer = timer;
+        init(0);
+    }
+
+    /**
+     *
+     * @param opMode
+     * @param timer
+     * @param visionMode
+     *          o: no camera is initialized
+     *          1: only armWebcam is initialized for OpenCV
+     *          2: backWebcam is initialized for Vuforia
+     *          3: backWebcam is initialized for Vuforia and frontWebcam is initialized for OpenCV
+     *          4: armWebcam is initialized for OpenCV and frontWebcam is initialized for OpenCV
+     */
+    public Robot(LinearOpMode opMode, ElapsedTime timer, int visionMode) throws IOException {
+        hardwareMap = opMode.hardwareMap;
+        this.opMode = opMode;
+        this.timer = timer;
+        init(visionMode);
+    }
+
+    public Robot () throws IOException {
+        init(0);
+    }
+
+    public void init() throws IOException {
+        init(0);
+    }
+
+    public void init(int visionMode) throws IOException {
+        //DC Motors
+        frontLeftDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("fl");
+        frontRightDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("fr");
+        rearLeftDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("bl");
+        rearRightDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("br");
+
+        frontRightDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRightDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeftDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearLeftDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearRightDriveMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+//        xRailWinch = (DcMotorEx) hardwareMap.dcMotor.get("winch");
+//        xRailWinch.setDirection(DcMotorSimple.Direction.REVERSE);
+//        xRailWinch.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        xRailWinch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        xRailWinch.setTargetPosition(0);
+//        xRailWinch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        xRailWinch.setPower(1.0);
+//
+//        armTilt = (DcMotorEx) hardwareMap.dcMotor.get("tilt");
+//        armTilt.setDirection(DcMotorSimple.Direction.FORWARD);
+//        armTilt.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        armTilt.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        armTilt.setTargetPosition(0);
+//        armTilt.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        armTilt.setPower(1.0);
+//
+//        //Servos
+//        mainClawArm = hardwareMap.servo.get("mA");
+//        mainClawRotation = hardwareMap.servo.get("mR");
+//        mainClaw = hardwareMap.servo.get("mC");
+//        csClaw = hardwareMap.servo.get("csC"); //capstone claw
+//        csArm = hardwareMap.servo.get("csA"); //capstone arm
+//        fClawL = hardwareMap.servo.get("fL");
+//        fClawR = hardwareMap.servo.get("fR");
+
+//        // Set servo scale ranges
+//        mainArm.scaleRange(0,1);
+//        mainRotation.scaleRange(0,1);
+//        mainClaw.scaleRange(0,1);
+//        fClawL.scaleRange(0.36,0.8);
+//        fClawR.scaleRange(0.11,0.63);
+
+        //Sensors
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+//        colorSensor = hardwareMap.colorSensor.get("color");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        opMode.telemetry.addData("Mode", " IMU initializing...");
+        opMode.telemetry.update();
+        imu.initialize(parameters);
+        opMode.telemetry.addData("Mode", " IMU calibrating...");
+        opMode.telemetry.update();
+        // make sure the imu gyro is calibrated before continuing.
+        while (opMode.opModeIsActive() && !imu.isGyroCalibrated())
+        {
+            opMode.sleep(50);
+            opMode.idle();
+        }
+
+        opMode.telemetry.addData("Mode", " drive/control initializing...");
+        opMode.telemetry.update();
+        //Subsystems
+        drive = new Drive(frontLeftDriveMotor, frontRightDriveMotor, rearLeftDriveMotor, rearRightDriveMotor, imu, opMode, timer);
+
+//        drive.setMotorKp(10.0, 10.0, 10.0, 10.0);
+//        drive.setMotorPID(5.0, 1.0, 1.0, 0.0);
+//        drive.printMotorPIDCoefficients();
+//        opMode.sleep(3000);
+
+        drive.setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        drive.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        drive.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        drive.initMaxVelocity();
+//        drive.stop();
+//        drive.printMotorPIDCoefficients();
+//        opMode.sleep(2000);
+
+        control = new Control(xRailWinch, armTilt, mainClaw, mainClawRotation, mainClawArm, csClaw, csArm, fClawL, fClawR, imu, opMode, timer);
+
+        if (visionMode != 0) {
+            opMode.telemetry.addData("Mode", " Camera initializing...");
+            opMode.telemetry.update();
+            vision = new Vision(hardwareMap, this, visionMode);
+        }
+    }
+
+    public void initServosAuto() {
+        // code here
+    }
+
+    public void initServosTeleop() {
+        // code here
+    }
+
+    public OpMode getOpmode(){
+        return this.opMode;
+    }
+
+    public void getGamePadInputs() {
+        isaButtonPressedPrev = aButton;
+        isbButtonPressedPrev = bButton;
+        isxButtonPressedPrev = xButton;
+        isyButtonPressedPrev = yButton;
+        isdPadUpPressedPrev = dPadUp;
+        isdPadDownPressedPrev = dPadDown;
+        isdPadLeftPressedPrev = dPadLeft;
+        isdPadRightPressedPrev = dPadRight;
+        islBumperPressedPrev = bumperLeft;
+        isrBumperPressedPrev = bumperRight;
+        leftStickX = joystickDeadzoneCorrection(opMode.gamepad1.left_stick_x);
+        leftStickY = joystickDeadzoneCorrection(-opMode.gamepad1.left_stick_y);
+        rightStickX = joystickDeadzoneCorrection(opMode.gamepad1.right_stick_x);
+        rightStickY = joystickDeadzoneCorrection(opMode.gamepad1.right_stick_y);
+        triggerLeft = opMode.gamepad1.left_trigger;
+        triggerRight = opMode.gamepad1.right_trigger;
+        aButton = opMode.gamepad1.a;
+        bButton = opMode.gamepad1.b;
+        xButton = opMode.gamepad1.x;
+        yButton = opMode.gamepad1.y;
+        dPadUp = opMode.gamepad1.dpad_up;
+        dPadDown = opMode.gamepad1.dpad_down;
+        dPadLeft = opMode.gamepad1.dpad_left;
+        dPadRight = opMode.gamepad1.dpad_right;
+        bumperLeft = opMode.gamepad1.left_bumper;
+        bumperRight = opMode.gamepad1.right_bumper;
+
+        isaButton2PressedPrev = aButton2;
+        isbButton2PressedPrev = bButton2;
+        isxButton2PressedPrev = xButton2;
+        isyButton2PressedPrev = yButton2;
+        isdPadUp2PressedPrev = dPadUp2;
+        isdPadDown2PressedPrev = dPadDown2;
+        isdPadLeft2PressedPrev = dPadLeft2;
+        isdPadRight2PressedPrev = dPadRight2;
+        islBumper2PressedPrev = bumperLeft2;
+        isrBumper2PressedPrev = bumperRight2;
+        leftStickX2 = joystickDeadzoneCorrection(opMode.gamepad2.left_stick_x);
+        leftStickY2 = joystickDeadzoneCorrection(-opMode.gamepad2.left_stick_y);
+        rightStickX2 = joystickDeadzoneCorrection(opMode.gamepad2.right_stick_x);
+        rightStickY2 = joystickDeadzoneCorrection(-opMode.gamepad2.right_stick_y);
+        triggerLeft2 = opMode.gamepad2.left_trigger;
+        triggerRight2 = opMode.gamepad2.right_trigger;
+        aButton2 = opMode.gamepad2.a;
+        bButton2 = opMode.gamepad2.b;
+        xButton2 = opMode.gamepad2.x;
+        yButton2 = opMode.gamepad2.y;
+        dPadUp2 = opMode.gamepad2.dpad_up;
+        dPadDown2 = opMode.gamepad2.dpad_down;
+        dPadLeft2 = opMode.gamepad2.dpad_left;
+        dPadRight2 = opMode.gamepad2.dpad_right;
+        bumperLeft2 = opMode.gamepad2.left_bumper;
+        bumperRight2 = opMode.gamepad2.right_bumper;
+    }
+
+    public double joystickDeadzoneCorrection(double joystickInput) {
+        double joystickOutput;
+        if (joystickInput > joystickDeadZone) {
+            joystickOutput = (joystickInput - joystickDeadZone) / (1.0-joystickDeadZone);
+        }
+        else if (joystickInput > -joystickDeadZone) {
+            joystickOutput = 0.0;
+        }
+        else {
+            joystickOutput = (joystickInput + joystickDeadZone) / (1.0-joystickDeadZone);
+        }
+        return joystickOutput;
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleopMark1.java
@@ -0,0 +1,302 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.SubSystems.Robot;
+
+import java.io.IOException;
+
+/**
+ * Created by PaigeYeung on 10/17/2020
+ */
+
+@TeleOp(name = "TeleopMark1")
+public class TeleopMark1 extends LinearOpMode {
+    //Declare DC motor objects
+    private Robot robot;
+
+
+
+//    double mainArmHorizontalPos = 0.0;
+//    double mainArmVerticalPos = 0.0;
+//    double mainArmHorizontalMax = 1000.0;
+//    double mainArmVerticalMax = 1100.0;
+//    double mainArmIncrement = 600.0;
+//    double mainClawRotationAngle;
+//    double mainClawRotationIncrement = 300;
+
+    double deltaT;
+    double timeCurrent;
+    double timePre;
+    ElapsedTime timer;
+
+//    private boolean mainClawArmControlDigital = true;
+//    private boolean mainClawArmDeployed = false;
+//    private boolean csClawArmControlDigital = true;
+//    private boolean csClawArmDeployed = false;
+
+    enum Prospective {
+        ROBOT,
+        DRIVER,
+    }
+
+    enum MainClawState {
+        CLOSE,
+        OPEN,
+        WIDEOPEN,
+    }
+    private MainClawState mainClawState;
+
+    private Prospective prospectiveMode = Prospective.ROBOT;
+    private double robotAngle;
+    private boolean visionEnabled = false;
+
+//    private int verticalStepsCount = 10;
+//    private double[] verticalSteps = {110.0, 210.0, 310.0, 410.0, 510.0, 603.0, 693.0, 790.0, 880.0, 970.0};
+
+    private void initOpMode() {
+        //Initialize DC motor objects
+        timer = new ElapsedTime();
+        if (visionEnabled) {
+            // visionMode 4: backWebcam is initialized for Vuforia and armWebcam is initialized for OpenCV
+            try {
+                robot = new Robot(this, timer, 4);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            try {
+                robot = new Robot(this, timer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        timeCurrent = timer.nanoseconds();
+        timePre = timeCurrent;
+
+        telemetry.addData("Wait for start", "");
+        telemetry.update();
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initOpMode();
+        waitForStart();
+
+        // call initServosTeleop() after running Auto program
+        robot.initServosTeleop();
+        // call initServosAuto() if testing Teleop stand-alone
+//        robot.initServosAuto();
+        mainClawRotationAngle = robot.control.getMainClawRotationDegrees();
+        telemetry.clearAll();
+        timeCurrent = timer.nanoseconds();
+        timePre = timeCurrent;
+        if (visionEnabled) {
+            robot.vision.getTargetsSkyStone().activate();
+        }
+        robot.control.closeMainClawStone();
+        mainClawState = MainClawState.CLOSE;
+        while(opModeIsActive()) {
+
+            // Get gamepad inputs
+            robot.getGamePadInputs();
+
+            // Get the current time
+            timeCurrent = timer.nanoseconds();
+            deltaT = timeCurrent - timePre;
+            timePre = timeCurrent;
+
+            if (visionEnabled) {
+                robot.vision.vuMarkScan();
+            }
+
+            // Drive the motors
+            double[] motorPowers;
+            robotAngle = robot.imu.getAngularOrientation().firstAngle;
+            if (prospectiveMode == Prospective.ROBOT) {
+                motorPowers = robot.drive.calcMotorPowers(robot.leftStickX, robot.leftStickY, robot.rightStickX);
+            }
+            else {  // DRIVER prospective mode
+                // Get robot angle
+                double relativeX = robot.leftStickX * Math.cos(robotAngle*Math.PI/180.0) + robot.leftStickY * Math.sin(robotAngle*Math.PI/180.0);
+                double relativeY = -robot.leftStickX * Math.sin(robotAngle*Math.PI/180.0) + robot.leftStickY * Math.cos(robotAngle*Math.PI/180.0);
+                motorPowers = robot.drive.calcMotorPowers(relativeX, relativeY, robot.rightStickX);
+            }
+            robot.drive.setDrivePowers(motorPowers);
+
+//            // move foundation claw
+//            if (robot.bumperLeft && (!robot.bumperRight)) { // foundation claw up
+//                robot.control.raiseClawsFromFoundation();
+//            } else if (robot.bumperRight && (!robot.bumperLeft)) { // foundation claw down
+//                robot.control.lowerClawsToFoundation();
+//            }
+//            if ((robot.triggerLeft > 0.5) && (robot.triggerRight < 0.5)) { // foundation claw up
+//                robot.control.raiseClawsFromFoundation();
+//            } else if ((robot.triggerRight > 0.5) && (robot.triggerLeft < 0.5)) { // foundation claw down
+//                robot.control.lowerClawsToFoundation();
+//            }
+//
+//            // deploy main claw arm
+//            if (mainClawArmControlDigital) {
+//                if (robot.bumperRight2 && !robot.isrBumper2PressedPrev) { // toggle main claw arm deploy mode
+//                    if (mainClawArmDeployed) {
+//                        robot.control.retractMainClawArm();
+//                        mainClawArmDeployed = false;
+//                    }
+//                    else {
+//                        robot.control.setMainClawArmDegrees(robot.control.getMainArmTargetAngle());
+//                        mainClawArmDeployed = true;
+//                    }
+//                }
+//            }
+//
+//            // deploy capstone claw arm
+//            if (csClawArmControlDigital) {
+//                if (robot.bumperLeft2 && !robot.islBumper2PressedPrev) { // toggle capstone claw arm deploy mode
+//                    if (csClawArmDeployed) {
+//                        robot.control.retractCSClawArm();
+//                        csClawArmDeployed = false;
+//                    }
+//                    else {
+//                        robot.control.setCSClawArmDegrees(robot.control.getMainArmTargetAngle());
+//                        csClawArmDeployed = true;
+//                    }
+//                }
+//            }
+;
+
+            // move robot main arm
+            // move robot main arm along horizontal line
+//            if(robot.leftStickY2 >= 0.1){
+//                mainArmHorizontalPos = mainArmHorizontalPos + (robot.leftStickY2 - 0.1) * (robot.leftStickY2 - 0.1) * mainArmIncrement * deltaT/1e9;
+//            }
+//            else if(robot.leftStickY2  <= -0.1){
+//                mainArmHorizontalPos = mainArmHorizontalPos - (robot.leftStickY2 + 0.1) * (robot.leftStickY2 + 0.1) * mainArmIncrement * deltaT/1e9;
+//            }
+//            if (mainArmHorizontalPos > mainArmHorizontalMax) {
+//                mainArmHorizontalPos = mainArmHorizontalMax;
+//            }
+//            if (mainArmHorizontalPos < 0.0) {
+//                mainArmHorizontalPos =0.0;
+//            }
+//            // move robot main arm along vertical line
+//            if(robot.rightStickY2 >= 0.1){
+//                mainArmVerticalPos = mainArmVerticalPos + (robot.rightStickY2 - 0.1) * (robot.rightStickY2 - 0.1) * mainArmIncrement * deltaT/1e9;
+//            }
+//            else if(robot.rightStickY2  <= -0.1){
+//                mainArmVerticalPos = mainArmVerticalPos - (robot.rightStickY2 + 0.1) * (robot.rightStickY2 + 0.1) * mainArmIncrement * deltaT/1e9;
+//            }
+//            if (mainArmVerticalPos > mainArmVerticalMax) {
+//                mainArmVerticalPos = mainArmVerticalMax;
+//            }
+//            if (mainArmVerticalPos < 0.0) {
+//                mainArmVerticalPos =0.0;
+//            }
+//            robot.control.setMainArmPosition(mainArmHorizontalPos, mainArmVerticalPos);
+//
+//            // rotate main claw
+//            if(robot.rightStickX2 >= 0.1){
+//                mainClawRotationAngle = mainClawRotationAngle + (robot.rightStickX2 - 0.1) * (robot.rightStickX2 - 0.1) * mainClawRotationIncrement * deltaT/1e9;
+//            }
+//            else if(robot.rightStickX2  <= -0.1){
+//                mainClawRotationAngle = mainClawRotationAngle - (robot.rightStickX2 + 0.1) * (robot.rightStickX2 + 0.1) * mainClawRotationIncrement * deltaT/1e9;
+//            }
+//            if (mainClawRotationAngle > 180.0) {
+//                mainClawRotationAngle = 180.0;
+//            }
+//            if (mainClawRotationAngle < 0.0) {
+//                mainClawRotationAngle =0.0;
+//            }
+//            robot.control.setMainClawRotationDegrees(mainClawRotationAngle);
+//
+//            if(mainClawArmControlDigital && mainClawArmDeployed){
+//                robot.control.setMainClawArmDegrees(robot.control.getMainArmTargetAngle());
+//            }
+//            if(csClawArmDeployed && csClawArmControlDigital){
+//                robot.control.setCSClawArmDegrees(robot.control.getMainArmTargetAngle());
+//            }
+//
+//            //Automate skybrige pos
+//            if(robot.bButton2 && !robot.isbButton2PressedPrev){
+//                mainArmHorizontalPos = 40.0;
+//                mainArmVerticalPos = 50.0;
+//                robot.control.setMainArmPosition(mainArmHorizontalPos, mainArmVerticalPos);
+//            }
+
+//            //Automate stone grabbing
+//            if(robot.xButton2 && !robot.isxButton2PressedPrev){
+//                if (mainArmVerticalPos < 150.0) { // only do this when the arm is low to prevent accidents
+//                    // make sure main claw is open
+//                    robot.control.openMainClaw();
+//
+//                    // move main arm down
+//                    mainArmVerticalPos = 0.0;
+//                    robot.control.setMainArmPosition(mainArmHorizontalPos, mainArmVerticalPos);
+//                    robot.control.setMainClawArmDegrees(robot.control.getMainArmTargetAngle());
+//                    sleep (200);
+//                    robot.control.closeMainClawStone();
+//                    sleep (400);
+//                    mainArmVerticalPos = 50.0;
+//                    robot.control.setMainArmPosition(mainArmHorizontalPos, mainArmVerticalPos);
+//                }
+//            }
+
+//            //Automate stone vertical stepping
+//            if(robot.dPadUp2 && !robot.isdPadUp2PressedPrev){
+//                int currentStep = 0;
+//                while ((currentStep < verticalStepsCount) && (mainArmVerticalPos + 15.0 > verticalSteps[currentStep])) {
+//                    currentStep = currentStep + 1;
+//                }
+//                if (currentStep < verticalStepsCount) {
+//                    mainArmVerticalPos = verticalSteps[currentStep];
+//                    robot.control.setMainArmPosition(mainArmHorizontalPos, mainArmVerticalPos);
+//                }
+//            }
+
+//            //Automate stone vertical stepping
+//            if(robot.dPadDown2 && !robot.isdPadDown2PressedPrev){
+//                int currentStep = verticalStepsCount - 1;
+//                while ((currentStep >= 0) && (mainArmVerticalPos - 15.0 < verticalSteps[currentStep])) {
+//                    currentStep = currentStep - 1;
+//                }
+//                if (currentStep >= 0) {
+//                    mainArmVerticalPos = verticalSteps[currentStep];
+//                    robot.control.setMainArmPosition(mainArmHorizontalPos, mainArmVerticalPos);
+//                }
+//            }
+
+            // reset drive motor encoders
+            if (robot.yButton && !robot.isyButtonPressedPrev) {
+                robot.drive.resetDriveMotorEncoders();
+            }
+
+            // toggle drive prospective mode
+            if (robot.xButton && !robot.isxButtonPressedPrev) {
+                if (prospectiveMode == Prospective.DRIVER) {
+                    prospectiveMode = Prospective.ROBOT;
+                }
+                else {
+                    prospectiveMode = Prospective.DRIVER;
+                }
+            }
+
+//            telemetry.addData("Arm ","X %.1f, Y %.1f", mainArmHorizontalPos, mainArmVerticalPos);
+//            telemetry.addData("Arm "," tilt %.0f, %.0f; length %.0f, %.0f",
+//                    robot.control.getMainArmAngleTickTarget(), robot.control.getMainArmAngleTickCurrent(),
+//                    robot.control.getMainArmLengthTickTarget(), robot.control.getMainArmLengthTickCurrent());
+            telemetry.addData("clawRotation", mainClawRotationAngle);
+            telemetry.addData("Drive Mode ", prospectiveMode.toString());
+            telemetry.addData("robot angle ", robotAngle);
+            int currentPositions[] = robot.drive.getCurrentPositions();
+            telemetry.addData("position", "fl %d, fr %d, rl %d, rr %d",
+                    currentPositions[0], currentPositions[1], currentPositions[2], currentPositions[3]);
+            telemetry.update();
+        }
+    }
+
+}

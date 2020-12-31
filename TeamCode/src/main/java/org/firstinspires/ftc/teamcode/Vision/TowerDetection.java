63ac1fbf82c77542c2794236a92686bb8841785e
Ryan Lin <rlin23205@gmail.com>
2020-12-31 10:58:38 -0800

    Added additional documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/TowerDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/TowerDetection.java
@@ -14,27 +14,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
-import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
 import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
 
 @TeleOp(name="Tower Detection")
 public class TowerDetection extends LinearOpMode {
-
-    // IMPORTANT: If you are using a USB WebCam, you must select CAMERA_CHOICE = BACK; and PHONE_IS_PORTRAIT = false;
-    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
-    private static final boolean PHONE_IS_PORTRAIT = false;
     private static final boolean isBlue = true;
 
     private static final String VUFORIA_KEY =
             "ATDGULf/////AAABmRRGSyLSbUY4lPoqBYjklpYqC4y9J7bCk42kjgYS5KtgpKL8FbpEDQTovzZG8thxB01dClvthxkSuSyCkaZi+JiD5Pu0cMVre3gDwRvwRXA7V9kpoYyMIPMVX/yBTGaW8McUaK9UeQUaFSepsTcKjX/itMtcy7nl1k84JChE4i8whbinHWDpaNwb5qcJsXlQwJhE8JE7t8NMxMm31AgzqjVf/7HwprTRfrxjTjVx5v2rp+wgLeeLTE/xk1JnL3fZMG6yyxPHgokWlIYEBZ5gBX+WJfgA+TDsdSPY/MnBp5Z7QxQsO9WJA59o/UzyEo/9BkbvYJZfknZqeoZWrJoN9jk9sivFh0wIPsH+JjZNFsPw";
+
     // Since ImageTarget trackables use mm to specifiy their dimensions, we must use mm for all the physical dimension.
-    // We will define some constants and conversions here
+    // Define constants
     private static final float mmPerInch        = 25.4f;
     private static final float mmTargetHeight   = (6) * mmPerInch;          // the height of the center of the target image above the floor
 
@@ -42,31 +35,26 @@ public class TowerDetection extends LinearOpMode {
     private OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia = null;
 
-    /**
-     * This is the webcam we are to use. As with other hardware devices such as motors and
-     * servos, this device is identified using the robot configuration tool in the FTC application.
-     */
     WebcamName webcamName = null;
 
     private boolean targetVisible = false;
 
     @Override public void runOpMode() {
 
-        //Retrieve webcame from hardwareMap
+        // Retrieve webcame from hardwareMap
         webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
 
-        //Configure parameters
+        // Configure parameters
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
         parameters.cameraName = webcamName; //Indicate which webcame should be used
         parameters.useExtendedTracking = false;
 
-        //  Instantiate the Vuforia engine
+        // Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
 
-        // Load the data sets for the trackable objects. These particular data
-        // sets are stored in the 'assets' part of our application.
+        // Load the data sets for the trackable objects. These particular data sets are stored in the 'assets' part of our application.
         VuforiaTrackables targetsUltimateGoal = this.vuforia.loadTrackablesFromAsset("UltimateGoal");
         VuforiaTrackable target;
         if(isBlue) {
@@ -77,30 +65,20 @@ public class TowerDetection extends LinearOpMode {
             target.setName("Red Tower Goal Target");
         }
 
-        //Set the position of the perimeter targets with relation to origin (in our case, this is the "sweet spot")
+        // Set the position of the perimeter targets with relation to origin in millimeters (in our case, this is the "sweet spot")
         target.setLocation(createMatrix(0, 500, mmTargetHeight, 90, 0, 0));
 
-        // Define where camera is in relation to center of robot
+        // Define where camera is in relation to center of robot in inches
         final float CAMERA_FORWARD_DISPLACEMENT  = 0 * mmPerInch;
         final float CAMERA_VERTICAL_DISPLACEMENT = 0 * mmPerInch;
         final float CAMERA_LEFT_DISPLACEMENT     = 0 * mmPerInch;
 
         OpenGLMatrix robotFromCamera = createMatrix(CAMERA_LEFT_DISPLACEMENT, CAMERA_FORWARD_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT, 90, 0, 0);
 
-        /**  Let all the trackable listeners know where the phone is.  */
         ((VuforiaTrackableDefaultListener) target.getListener()).setPhoneInformation(robotFromCamera, parameters.cameraDirection);
 
-        // WARNING:
-        // In this sample, we do not wait for PLAY to be pressed.  Target Tracking is started immediately when INIT is pressed.
-        // This sequence is used to enable the new remote DS Camera Preview feature to be used with this sample.
-        // CONSEQUENTLY do not put any driving commands in this loop.
-        // To restore the normal opmode structure, just un-comment the following line:
-
-        // waitForStart();
-
-        // Note: To use the remote camera preview:
-        // AFTER you hit Init on the Driver Station, use the "options menu" to select "Camera Stream"
-        // Tap the preview window to receive a fresh image.
+        // Comment the line below to enable Camera Preview
+        waitForStart();
 
         targetsUltimateGoal.activate();
         while (!isStopRequested()) {
@@ -140,6 +118,7 @@ public class TowerDetection extends LinearOpMode {
         targetsUltimateGoal.deactivate();
     }
 
+    // Helper method to create matrix to identify locations
     public OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w) {
         return OpenGLMatrix.translation(x, y, z)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, u, v , w));
